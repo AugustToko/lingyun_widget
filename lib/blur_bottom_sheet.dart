370dc0ebc12ce29abe221a77c86fe211a7403f00
GiKode <827266641@qq.com>
2021-01-16 10:13:00 +0800

    update
    Signed-off-by: GiKode <827266641@qq.com>

---
+++ lib/blur_bottom_sheet.dart
@@ -0,0 +1,200 @@
+/*
+ * Project: tools_pack
+ * Module: toolspack
+ * Last Modified: 20-9-28 下午4:16
+ * Copyright (c) 2020 August https://blog.geek-cloud.top/
+ */
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class MyModalBottomSheetRoute<T> extends PopupRoute<T> {
+  MyModalBottomSheetRoute({
+    this.builder,
+    this.theme,
+    this.barrierLabel,
+    this.backgroundColor,
+    this.elevation,
+    this.shape,
+    this.clipBehavior,
+    this.isDismissible = true,
+    this.isBlurred = false,
+    @required this.isScrollControlled,
+    RouteSettings settings,
+  })  : assert(isScrollControlled != null),
+        assert(isDismissible != null),
+        super(
+            settings: settings,
+            filter:
+                isBlurred ? ImageFilter.blur(sigmaY: 32, sigmaX: 32) : null);
+
+  final WidgetBuilder builder;
+  final ThemeData theme;
+  final bool isScrollControlled;
+  final Color backgroundColor;
+  final double elevation;
+  final ShapeBorder shape;
+  final Clip clipBehavior;
+  final bool isDismissible;
+  final bool isBlurred;
+
+  @override
+  Duration get transitionDuration => Duration(milliseconds: 200);
+
+  @override
+  bool get barrierDismissible => isDismissible;
+
+  @override
+  final String barrierLabel;
+
+  @override
+  Color get barrierColor => Colors.white.withOpacity(0.7);
+
+  AnimationController _animationController;
+
+  @override
+  AnimationController createAnimationController() {
+    assert(_animationController == null);
+    _animationController =
+        BottomSheet.createAnimationController(navigator.overlay);
+    return _animationController;
+  }
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    final BottomSheetThemeData sheetTheme =
+        theme?.bottomSheetTheme ?? Theme.of(context).bottomSheetTheme;
+    // By definition, the bottom sheet is aligned to the bottom of the page
+    // and isn't exposed to the top padding of the MediaQuery.
+    Widget bottomSheet = MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      child: _ModalBottomSheet<T>(
+        route: this,
+        backgroundColor: backgroundColor ??
+            sheetTheme?.modalBackgroundColor ??
+            sheetTheme?.backgroundColor,
+        elevation:
+            elevation ?? sheetTheme?.modalElevation ?? sheetTheme?.elevation,
+        shape: shape,
+        clipBehavior: clipBehavior,
+        isScrollControlled: isScrollControlled,
+      ),
+    );
+    if (theme != null) bottomSheet = Theme(data: theme, child: bottomSheet);
+    return bottomSheet;
+  }
+}
+
+class _ModalBottomSheet<T> extends StatefulWidget {
+  const _ModalBottomSheet({
+    Key key,
+    this.route,
+    this.backgroundColor,
+    this.elevation,
+    this.shape,
+    this.clipBehavior,
+    this.isScrollControlled = false,
+  })  : assert(isScrollControlled != null),
+        super(key: key);
+
+  final MyModalBottomSheetRoute<T> route;
+  final bool isScrollControlled;
+  final Color backgroundColor;
+  final double elevation;
+  final ShapeBorder shape;
+  final Clip clipBehavior;
+
+  @override
+  _ModalBottomSheetState<T> createState() => _ModalBottomSheetState<T>();
+}
+
+class _ModalBottomSheetState<T> extends State<_ModalBottomSheet<T>> {
+  String _getRouteLabel(MaterialLocalizations localizations) {
+    switch (Theme.of(context).platform) {
+      case TargetPlatform.iOS:
+        return '';
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+        return localizations.dialogLabel;
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMediaQuery(context));
+    assert(debugCheckHasMaterialLocalizations(context));
+    final MediaQueryData mediaQuery = MediaQuery.of(context);
+    final MaterialLocalizations localizations =
+        MaterialLocalizations.of(context);
+    final String routeLabel = _getRouteLabel(localizations);
+
+    return AnimatedBuilder(
+      animation: widget.route.animation,
+      builder: (BuildContext context, Widget child) {
+        // Disable the initial animation when accessible navigation is on so
+        // that the semantics are added to the tree at the correct time.
+        final double animationValue = mediaQuery.accessibleNavigation
+            ? 1.0
+            : widget.route.animation.value;
+        return Semantics(
+          scopesRoute: true,
+          namesRoute: true,
+          label: routeLabel,
+          explicitChildNodes: true,
+          child: ClipRect(
+            child: CustomSingleChildLayout(
+              delegate: _ModalBottomSheetLayout(
+                  animationValue, widget.isScrollControlled),
+              child: BottomSheet(
+                animationController: widget.route._animationController,
+                onClosing: () {
+                  if (widget.route.isCurrent) {
+                    Navigator.pop(context);
+                  }
+                },
+                builder: widget.route.builder,
+                backgroundColor: widget.backgroundColor,
+                elevation: widget.elevation,
+                shape: widget.shape,
+                clipBehavior: widget.clipBehavior,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ModalBottomSheetLayout extends SingleChildLayoutDelegate {
+  _ModalBottomSheetLayout(this.progress, this.isScrollControlled);
+
+  final double progress;
+  final bool isScrollControlled;
+
+  @override
+  BoxConstraints getConstraintsForChild(BoxConstraints constraints) {
+    return BoxConstraints(
+      minWidth: constraints.maxWidth,
+      maxWidth: constraints.maxWidth,
+      minHeight: 0.0,
+      maxHeight: isScrollControlled
+          ? constraints.maxHeight
+          : constraints.maxHeight * 9.0 / 16.0,
+    );
+  }
+
+  @override
+  Offset getPositionForChild(Size size, Size childSize) {
+    return Offset(0.0, size.height - childSize.height * progress);
+  }
+
+  @override
+  bool shouldRelayout(_ModalBottomSheetLayout oldDelegate) {
+    return progress != oldDelegate.progress;
+  }
+}
---
+++ lib/blur_dialog.dart
@@ -0,0 +1,75 @@
+/*
+ * Project: tools_pack
+ * Module: toolspack
+ * Last Modified: 20-9-13 上午10:11
+ * Copyright (c) 2020 August https://blog.geek-cloud.top/
+ */
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+/// 模糊背景 Dialog
+class MyBlurDialogRoute<T> extends PopupRoute<T> {
+  MyBlurDialogRoute({
+    @required RoutePageBuilder pageBuilder,
+    bool barrierDismissible = true,
+    String barrierLabel,
+    Duration transitionDuration = const Duration(milliseconds: 200),
+    RouteTransitionsBuilder transitionBuilder,
+    RouteSettings settings,
+  })  : assert(barrierDismissible != null),
+        _pageBuilder = pageBuilder,
+        _barrierDismissible = barrierDismissible,
+        _barrierLabel = barrierLabel,
+        _transitionDuration = transitionDuration,
+        _transitionBuilder = transitionBuilder,
+//        super(settings: settings, filter: BlurSettings.blurFilter);
+        super(
+            settings: settings,
+            filter: ImageFilter.blur(sigmaX: 60, sigmaY: 60));
+
+  final RoutePageBuilder _pageBuilder;
+
+  @override
+  bool get barrierDismissible => _barrierDismissible;
+  final bool _barrierDismissible;
+
+  @override
+  String get barrierLabel => _barrierLabel;
+  final String _barrierLabel;
+
+  @override
+  Color get barrierColor => _barrierColor;
+  final Color _barrierColor = Colors.white.withOpacity(0.05);
+
+  @override
+  Duration get transitionDuration => _transitionDuration;
+  final Duration _transitionDuration;
+
+  final RouteTransitionsBuilder _transitionBuilder;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return Semantics(
+      child: _pageBuilder(context, animation, secondaryAnimation),
+      scopesRoute: true,
+      explicitChildNodes: true,
+    );
+  }
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (_transitionBuilder == null) {
+      return FadeTransition(
+          opacity: CurvedAnimation(
+            parent: animation,
+            curve: Curves.linear,
+          ),
+          child: child);
+    } // Some default transition
+    return _transitionBuilder(context, animation, secondaryAnimation, child);
+  }
+}
