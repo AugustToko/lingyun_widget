cc850d78e373013e22e33e830f1494853f853989
GiKode <827266641@qq.com>
2021-01-16 10:55:49 +0800

    update
    Signed-off-by: GiKode <827266641@qq.com>

---
+++ lib/loading_dialog.dart
@@ -0,0 +1,56 @@
+/*
+ * Project: tools_pack
+ * Module: toolspack
+ * Last Modified: 20-5-6 下午4:06
+ * Copyright (c) 2020 August https://blog.geek-cloud.top/
+ */
+
+import 'package:flutter/material.dart';
+
+@deprecated
+class LoadingDialog extends Dialog {
+  final String text;
+
+  final bool cancelable;
+
+  LoadingDialog({Key key, @required this.text, this.cancelable = false,})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: Material(
+          type: MaterialType.transparency,
+          child: Center(
+            child: SizedBox(
+              height: 120.0,
+              child: Container(
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8.0),
+                    ),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding:
+                      const EdgeInsets.only(top: 20.0, left: 10, right: 10),
+                      child: Text(text),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        onWillPop: () {
+          return Future.value(cancelable);
+        });
+  }
+}
